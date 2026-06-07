d6faa35da4d054d38629f19df7037d7cb4d82e3c
Bizimana Eric <ericbizimana111@gmail.com>
2026-06-07 20:12:29 +0200

    added codes

---
+++ frontend/src/shared/components/charts/index.ts
@@ -0,0 +1,4 @@
+export { RevenueChart } from './RevenueChart';
+export { OrdersChart } from './OrdersChart';
+export { default as RevenueChartDefault } from './RevenueChart';
+export { default as OrdersChartDefault } from './OrdersChart';
